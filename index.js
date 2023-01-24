5660fc76b6995f1bf50752144b477985e629afe8
vishalicious213 <vishalicious213@gmail.com>
2023-01-24 14:23:53 -0800

    logging out all of the emotionTags from catsData using nested for...of loops

--- index.js
+++ index.js
@@ -171,9 +171,15 @@ const catsData = [
 
 function getEmotionsArray(cats){
     const emotionsArray = []
-    for (let i = 0; i < cats.length; i++){
-        for (let j=0; j < cats[i].emotionTags.length; j++){
-                emotionsArray.push(cats[i].emotionTags[j])
+    // for (let i = 0; i < cats.length; i++){
+    //     for (let j=0; j < cats[i].emotionTags.length; j++){
+    //             emotionsArray.push(cats[i].emotionTags[j])
+    //     }
+    // }
+
+    for (let cat of cats) {
+        for (let emotion of cat.emotionTags) {
+            console.log(emotion)
         }
     }
 console.log(emotionsArray)
