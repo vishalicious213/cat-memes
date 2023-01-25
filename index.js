d6f140b65b50fbd0c37d51427233e373594c6649
vishalicious213 <vishalicious213@gmail.com>
2023-01-24 17:46:34 -0800

    rendering emotions as inputs with a label. there are duplicates that need to be addressed

--- index.js
+++ index.js
@@ -21,7 +21,18 @@ function renderEmotionsRadios(cats) {
     let radioItems = ``
 
     for (let emotion of emotions) {
-        radioItems += `<p>${emotion}</p>`
+        // radioItems += `<p>${emotion}</p>`
+        radioItems += `
+            <div class="radio">
+                <label for="${emotion}">${emotion}</label>
+                <input 
+                    type="radio"
+                    id="${emotion}"
+                    value="${emotion}"
+                    name="emotions"
+                >
+            </div>
+        `
     }
 
     emotionRadiosDiv.innerHTML = radioItems
