92a039e170880267eb629470521e8e7bef5af1ce
vishalicious213 <vishalicious213@gmail.com>
2023-01-25 21:43:42 -0800

    reorganized code. drafted getSingleCatObject() and renderCat(). changed getImgBtn event listener to call renderCat, which calls getSingleCarObject, which calls getMatchingCatsArray

--- index.js
+++ index.js
@@ -5,39 +5,9 @@ const getImageBtn = document.getElementById("get-image-btn")
 const gifsOnlyCheckbox = document.getElementById("gifs-only-option")
 
 emotionRadiosDiv.addEventListener("change", highlightCheckedOption)
+getImageBtn.addEventListener("click", renderCat)
 
-getImageBtn.addEventListener("click", getMatchingCatsArray)
-
-function highlightCheckedOption(e) {
-    // remove highlight from all radio buttons
-    const radioButtons = document.getElementsByClassName("radio")
-    for (let radioButton of radioButtons) {
-        radioButton.classList.remove("highlight")
-    }
-
-    // add highlight to selected radio button
-    document.getElementById(e.target.id).parentElement.classList.add("highlight")
-}
-
-// return array of cats that fit selected mood and .gif selection
-function getMatchingCatsArray() {
-    // get emotion if one is selected
-    if (document.querySelector("input[type='radio']:checked")) {
-        const checkedEmotion = document.querySelector("input[type='radio']:checked").value
-        const onlyGifs = gifsOnlyCheckbox.checked
-
-        // filter cat for emotion and whether it's a .gif
-        const matchingCatsArray = catsData.filter(function(cat) {
-            if (onlyGifs) {
-                return (cat.emotionTags.includes(checkedEmotion) && cat.isGif)
-            } else {
-                return cat.emotionTags.includes(checkedEmotion)
-            }
-        })
-
-        return matchingCatsArray
-    }
-}
+// RENDER EMOTIONS LIST & RADIO BUTTONS
 
 // return emotions from array of data
 function getEmotionsArray(cats){
@@ -74,6 +44,48 @@ function renderEmotionsRadios(cats) {
     }
 
     emotionRadiosDiv.innerHTML = radioItems
+}
+
+// highlight emotion whose radio button is clicked
+function highlightCheckedOption(e) {
+    // remove highlight from all radio buttons
+    const radioButtons = document.getElementsByClassName("radio")
+    for (let radioButton of radioButtons) {
+        radioButton.classList.remove("highlight")
+    }
+
+    // add highlight to selected radio button
+    document.getElementById(e.target.id).parentElement.classList.add("highlight")
+}
+
+// RENDER SELECTED CAT GIF
+
+// return array of cats that fit selected mood and .gif selection
+function getMatchingCatsArray() {
+    // get emotion if one is selected
+    if (document.querySelector("input[type='radio']:checked")) {
+        const checkedEmotion = document.querySelector("input[type='radio']:checked").value
+        const onlyGifs = gifsOnlyCheckbox.checked
+
+        // filter cat for emotion and whether it's a .gif
+        const matchingCatsArray = catsData.filter(function(cat) {
+            if (onlyGifs) {
+                return (cat.emotionTags.includes(checkedEmotion) && cat.isGif)
+            } else {
+                return cat.emotionTags.includes(checkedEmotion)
+            }
+        })
+
+        return matchingCatsArray
+    }
+}
+
+function getSingleCatObject() {
+    console.log(getMatchingCatsArray())
+}
+
+function renderCat() {
+    getSingleCatObject()
 }
 
 renderEmotionsRadios(catsData)
